9457e7ab4b66b3a7a2e717d8e659fad57f80c922
Imran Juma <imranjuma49@gmail.com>
2018-04-12 18:01:52 -0400

    First Upload
    Just me setting up the project, I will upload at every major acomplishment to keep track of different versions!

---
+++ PressMe/PressMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PressMe
+//
+//  Created by Imran Juma on 2018-04-12.
+//  Copyright © 2018 Imran Juma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
